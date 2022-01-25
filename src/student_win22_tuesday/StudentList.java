c3c85b25549a987a36b06f6fa3628b66a4260f32
koroll <koroll@sheridancollege.ca>
2022-01-25 14:11:34 -0500

    Revert "array size changed from 3 to 4"
    This reverts commit 79e7a9688b39849683fbf1375ccecedf6f39c0ec.

--- src/student_win22_tuesday/StudentList.java
+++ src/student_win22_tuesday/StudentList.java
@@ -4,7 +4,7 @@ package student_win22_tuesday;
 
 public class StudentList {
     public static void main(String[] args) {
-        Student[] studentList = new Student[4];
+        Student[] studentList = new Student[3];
         
         studentList[0] = new Student("s1","Lili");
         studentList[0] = new Student("s2","Lili");
